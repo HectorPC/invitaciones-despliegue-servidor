60a6440fb65f7a8034f6fc754f85b421455d5c92
LAPTOP-casa\Héctor <wifly00@hotmail.com>
2018-12-30 20:25:36 +0100

    news style and images

---
+++ precache-manifest.0270b9d4c4a0b02de81085d5ae643815.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fdd7cc54d7bf1bffdbc8f203a03c7ff",
+    "url": "/assets/backgrounds/imgsfuente/alfombraFreeNoOp.png"
+  },
+  {
+    "revision": "4d925920c0c73b8ed6a0",
+    "url": "/css/app.d8045ee3.css"
+  },
+  {
+    "revision": "31488ae7a87bc9e7d3b5",
+    "url": "/js/chunk-vendors.7ca3f42a.js"
+  },
+  {
+    "revision": "59347948cdeff2851065dc1cf028a712",
+    "url": "/img/calendar.59347948.jpg"
+  },
+  {
+    "revision": "390526c96857623392ef0ec870539a99",
+    "url": "/img/copy-icon.390526c9.png"
+  },
+  {
+    "revision": "aa2e5943fb04f92007d1c4780f27bde6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d925920c0c73b8ed6a0",
+    "url": "/js/app.4260f9f3.js"
+  },
+  {
+    "revision": "5ab531088eb4d7d2b9a068c37513d240",
+    "url": "/assets/backgrounds/imgsfuente/rezarNoOp.jpg"
+  },
+  {
+    "revision": "390526c96857623392ef0ec870539a99",
+    "url": "/assets/img/copy-icon.png"
+  },
+  {
+    "revision": "24c7731f687d2ad540d9483c36d87fc0",
+    "url": "/assets/backgrounds/imgsfuente/solNoOp.jpg"
+  },
+  {
+    "revision": "9982b459f307fb50165bb97689a832de",
+    "url": "/assets/backgrounds/weddingFree.png"
+  },
+  {
+    "revision": "59347948cdeff2851065dc1cf028a712",
+    "url": "/assets/img/calendar.jpg"
+  },
+  {
+    "revision": "7416fe6808d40cd6fd4270eefd058068",
+    "url": "/assets/backgrounds/imgsfuente/corazonFreeNoOp.jpg"
+  },
+  {
+    "revision": "da3dc9dc9d07c1276c310079ee37bfbd",
+    "url": "/assets/backgrounds/baloon-image-17.jpg"
+  },
+  {
+    "revision": "fff31d3e0bf3a9a25875a4668bf52239",
+    "url": "/assets/backgrounds/baloonFree2.png"
+  },
+  {
+    "revision": "59347948cdeff2851065dc1cf028a712",
+    "url": "/assets/backgrounds/calendar.jpg"
+  },
+  {
+    "revision": "7ba1c3cf3ba3003e3d698ed59f0c2ff2",
+    "url": "/assets/backgrounds/comunionFree.jpg"
+  },
+  {
+    "revision": "4e72240475623a1d3eea4b6f44955d8b",
+    "url": "/assets/backgrounds/copas.jpg"
+  },
+  {
+    "revision": "083a95d23efb0fc71e7a7a37853d9047",
+    "url": "/assets/backgrounds/corazonFree.jpg"
+  },
+  {
+    "revision": "8f52db93a87b7b85c35817ba42639e75",
+    "url": "/assets/backgrounds/globosColores1.jpg"
+  },
+  {
+    "revision": "6a454e6a34a48d9fc8086aa4a4699a44",
+    "url": "/assets/backgrounds/fuegosArtificiales.jpg"
+  },
+  {
+    "revision": "1df593cff1437c3b612545aafd9dc3ec",
+    "url": "/assets/backgrounds/globosCorazon.jpg"
+  },
+  {
+    "revision": "1b610b12efbb265889ce4c6403e3272b",
+    "url": "/assets/backgrounds/imgsfuente/baloonFree2NoOp.png"
+  },
+  {
+    "revision": "2ffa5eee43971102c3a5afd4b4086814",
+    "url": "/assets/backgrounds/imgsfuente/weddingFree NoOp.png"
+  },
+  {
+    "revision": "67bfb4e9ca6e5e09aa0bdd663d497cfd",
+    "url": "/assets/backgrounds/imgsfuente/BordeChupetes NoOp.jpg"
+  },
+  {
+    "revision": "b2a274223b307d16b3399fb2790b5d56",
+    "url": "/assets/backgrounds/imgsfuente/comunionFreeNoOp.jpg"
+  },
+  {
+    "revision": "3e2d2196c14389c2365e9a2935b4801a",
+    "url": "/assets/backgrounds/imgsfuente/conferenciaNoOp.jpg"
+  },
+  {
+    "revision": "d8e2d32d448f0cf7231e01a18319afbd",
+    "url": "/assets/backgrounds/piesBebeYmanos.jpg"
+  },
+  {
+    "revision": "b9c0e3dfd0e0ca38a3e56197eb1d1875",
+    "url": "/assets/backgrounds/imgsfuente/manosNoOp.jpg"
+  },
+  {
+    "revision": "ce4b142962ccb031ae565905f5cf926f",
+    "url": "/assets/backgrounds/imgsfuente/nubesNoOp.jpg"
+  },
+  {
+    "revision": "f29c7aacfdd1a27ca8b6bebb92bb0e9e",
+    "url": "/assets/backgrounds/imgsfuente/numerosCalendarioNoOp.jpg"
+  },
+  {
+    "revision": "59da1e994e41f207e0d3628c426b3101",
+    "url": "/assets/backgrounds/velasTarta2.jpg"
+  },
+  {
+    "revision": "d1dce146a19595ab1ac862fed944430b",
+    "url": "/assets/backgrounds/baloon-image-47.jpg"
+  },
+  {
+    "revision": "9f36d6fdedbcaf374db989e5e4c5e922",
+    "url": "/assets/backgrounds/up2.png"
+  },
+  {
+    "revision": "1d9c697ba3a449f3b9d7291113f52d7b",
+    "url": "/assets/backgrounds/velasTarta1.jpg"
+  },
+  {
+    "revision": "931e25b2c8942555037eda01859db75b",
+    "url": "/assets/backgrounds/logoV01.png"
+  },
+  {
+    "revision": "725e1f11703aabee8611c76d1990accb",
+    "url": "/assets/backgrounds/manos.jpg"
+  },
+  {
+    "revision": "fd571615708ccb5da331cda5851e9d7a",
+    "url": "/assets/backgrounds/marcoGlobos.png"
+  },
+  {
+    "revision": "94d158fda1a61559e229fcea3b4014a8",
+    "url": "/assets/backgrounds/imgsfuente/baloonFreeNoOP.png"
+  },
+  {
+    "revision": "3c87be111e5f30c7b2282f345ac55b94",
+    "url": "/assets/backgrounds/multitud.jpg"
+  },
+  {
+    "revision": "0869fb73d25bfa9b7e5181038966aa2e",
+    "url": "/assets/backgrounds/paraPruebas.png"
+  },
+  {
+    "revision": "9352af7911e623f49586d13a36c1c2fb",
+    "url": "/assets/backgrounds/nubes.jpg"
+  },
+  {
+    "revision": "e6b565b977d10d619ea6d174ad9966c1",
+    "url": "/assets/backgrounds/baloonFree.png"
+  },
+  {
+    "revision": "700cd81ff6b11119e73340b00217f403",
+    "url": "/assets/backgrounds/salonCelebracion2.jpg"
+  },
+  {
+    "revision": "221f98ebe0295b92ebbde83e97b0f640",
+    "url": "/assets/backgrounds/rezar.jpg"
+  },
+  {
+    "revision": "7b3a53f7c8911bfb86e14d4732267348",
+    "url": "/assets/backgrounds/regaloCumple.jpg"
+  },
+  {
+    "revision": "7ebdabf70f9dea145b98493415670b2e",
+    "url": "/assets/backgrounds/salonCelebracion1.jpg"
+  },
+  {
+    "revision": "ffcde3ad040f88a45c1ed75e11f1f766",
+    "url": "/assets/backgrounds/sol.jpg"
+  },
+  {
+    "revision": "014608ab19ff4903674c8804457ee753",
+    "url": "/assets/backgrounds/balloons-background.png"
+  },
+  {
+    "revision": "26843dfce436e15a809c841b471f8343",
+    "url": "/assets/backgrounds/balloons-1869790_1280.jpg"
+  },
+  {
+    "revision": "e9fed2a77c156b40be7fe1203493f7c7",
+    "url": "/assets/backgrounds/alfombraFree.png"
+  },
+  {
+    "revision": "6b0d4342d65e06c50ebd483c96776cdb",
+    "url": "/assets/backgrounds/Red-Carpet-alfombra-roja.jpg"
+  },
+  {
+    "revision": "ff891c5a018126e8ba2a79aba35672cb",
+    "url": "/assets/backgrounds/La-Alfombra-Roja.jpg"
+  },
+  {
+    "revision": "0809713f0bcc64a60646122cb216aae2",
+    "url": "/assets/backgrounds/BordeChupetes.jpg"
+  },
+  {
+    "revision": "4aa9ac9c3f226bfe8a745a1cf17e2781",
+    "url": "/assets/backgrounds/BabyFeet.jpg"
+  }
+];
