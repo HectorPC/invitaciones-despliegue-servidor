107dce9e9976c4db5185cac4df1b7ec91d2bcca6
LAPTOP-casa\Héctor <wifly00@hotmail.com>
2019-01-12 12:26:39 +0100

    commented adsense, display none previsualizar, play store link in footer

--- precache-manifest.9653dc7df0223a6cbdf169efde1d4483.js
+++ precache-manifest.9653dc7df0223a6cbdf169efde1d4483.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9fdd7cc54d7bf1bffdbc8f203a03c7ff",
-    "url": "/assets/backgrounds/imgsfuente/alfombraFreeNoOp.png"
+    "revision": "b9c0e3dfd0e0ca38a3e56197eb1d1875",
+    "url": "/assets/backgrounds/imgsfuente/manosNoOp.jpg"
   },
   {
-    "revision": "7b7b7d5646fd1367f94e",
-    "url": "/css/app.8f41d39a.css"
+    "revision": "a9b3d6a74785f2eb75b3",
+    "url": "/css/app.54a5eaa6.css"
   },
   {
     "revision": "31488ae7a87bc9e7d3b5",
@@ -20,12 +20,12 @@ self.__precacheManifest = [
     "url": "/img/copy-icon.390526c9.png"
   },
   {
-    "revision": "7e06b7307d383859cb06c3c2b3248294",
+    "revision": "75044f5e25a9d624bb6ade0357c33309",
     "url": "/index.html"
   },
   {
-    "revision": "7b7b7d5646fd1367f94e",
-    "url": "/js/app.47e6b7b8.js"
+    "revision": "a9b3d6a74785f2eb75b3",
+    "url": "/js/app.d91bbb39.js"
   },
   {
     "revision": "f29c7aacfdd1a27ca8b6bebb92bb0e9e",
@@ -36,8 +36,8 @@ self.__precacheManifest = [
     "url": "/assets/img/copy-icon.png"
   },
   {
-    "revision": "24c7731f687d2ad540d9483c36d87fc0",
-    "url": "/assets/backgrounds/imgsfuente/solNoOp.jpg"
+    "revision": "5ab531088eb4d7d2b9a068c37513d240",
+    "url": "/assets/backgrounds/imgsfuente/rezarNoOp.jpg"
   },
   {
     "revision": "9982b459f307fb50165bb97689a832de",
@@ -48,12 +48,16 @@ self.__precacheManifest = [
     "url": "/assets/img/calendar.jpg"
   },
   {
-    "revision": "5ab531088eb4d7d2b9a068c37513d240",
-    "url": "/assets/backgrounds/imgsfuente/rezarNoOp.jpg"
+    "revision": "24c7731f687d2ad540d9483c36d87fc0",
+    "url": "/assets/backgrounds/imgsfuente/solNoOp.jpg"
   },
   {
-    "revision": "d1dce146a19595ab1ac862fed944430b",
-    "url": "/assets/backgrounds/baloon-image-47.jpg"
+    "revision": "da3dc9dc9d07c1276c310079ee37bfbd",
+    "url": "/assets/backgrounds/baloon-image-17.jpg"
+  },
+  {
+    "revision": "0809713f0bcc64a60646122cb216aae2",
+    "url": "/assets/backgrounds/bordeChupetes.jpg"
   },
   {
     "revision": "fff31d3e0bf3a9a25875a4668bf52239",
@@ -64,16 +68,12 @@ self.__precacheManifest = [
     "url": "/assets/backgrounds/comunionFree.jpg"
   },
   {
-    "revision": "59347948cdeff2851065dc1cf028a712",
-    "url": "/assets/backgrounds/calendar.jpg"
+    "revision": "083a95d23efb0fc71e7a7a37853d9047",
+    "url": "/assets/backgrounds/corazonFree.jpg"
   },
   {
     "revision": "4e72240475623a1d3eea4b6f44955d8b",
     "url": "/assets/backgrounds/copas.jpg"
-  },
-  {
-    "revision": "083a95d23efb0fc71e7a7a37853d9047",
-    "url": "/assets/backgrounds/corazonFree.jpg"
   },
   {
     "revision": "8f52db93a87b7b85c35817ba42639e75",
@@ -84,72 +84,64 @@ self.__precacheManifest = [
     "url": "/assets/backgrounds/fuegosArtificiales.jpg"
   },
   {
-    "revision": "1df593cff1437c3b612545aafd9dc3ec",
-    "url": "/assets/backgrounds/globosCorazon.jpg"
-  },
-  {
     "revision": "1b610b12efbb265889ce4c6403e3272b",
     "url": "/assets/backgrounds/imgsfuente/baloonFree2NoOp.png"
+  },
+  {
+    "revision": "1df593cff1437c3b612545aafd9dc3ec",
+    "url": "/assets/backgrounds/globosCorazon.jpg"
   },
   {
     "revision": "67bfb4e9ca6e5e09aa0bdd663d497cfd",
     "url": "/assets/backgrounds/imgsfuente/BordeChupetes NoOp.jpg"
   },
   {
-    "revision": "d8e2d32d448f0cf7231e01a18319afbd",
-    "url": "/assets/backgrounds/piesBebeYmanos.jpg"
-  },
-  {
     "revision": "b2a274223b307d16b3399fb2790b5d56",
     "url": "/assets/backgrounds/imgsfuente/comunionFreeNoOp.jpg"
-  },
-  {
-    "revision": "3e2d2196c14389c2365e9a2935b4801a",
-    "url": "/assets/backgrounds/imgsfuente/conferenciaNoOp.jpg"
   },
   {
     "revision": "2ffa5eee43971102c3a5afd4b4086814",
     "url": "/assets/backgrounds/imgsfuente/weddingFree NoOp.png"
   },
   {
-    "revision": "7416fe6808d40cd6fd4270eefd058068",
-    "url": "/assets/backgrounds/imgsfuente/corazonFreeNoOp.jpg"
+    "revision": "3e2d2196c14389c2365e9a2935b4801a",
+    "url": "/assets/backgrounds/imgsfuente/conferenciaNoOp.jpg"
   },
   {
-    "revision": "b9c0e3dfd0e0ca38a3e56197eb1d1875",
-    "url": "/assets/backgrounds/imgsfuente/manosNoOp.jpg"
+    "revision": "d1dce146a19595ab1ac862fed944430b",
+    "url": "/assets/backgrounds/baloon-image-47.jpg"
+  },
+  {
+    "revision": "9fdd7cc54d7bf1bffdbc8f203a03c7ff",
+    "url": "/assets/backgrounds/imgsfuente/alfombraFreeNoOp.png"
   },
   {
     "revision": "ce4b142962ccb031ae565905f5cf926f",
     "url": "/assets/backgrounds/imgsfuente/nubesNoOp.jpg"
   },
   {
-    "revision": "9f36d6fdedbcaf374db989e5e4c5e922",
-    "url": "/assets/backgrounds/up2.png"
+    "revision": "7416fe6808d40cd6fd4270eefd058068",
+    "url": "/assets/backgrounds/imgsfuente/corazonFreeNoOp.jpg"
   },
   {
     "revision": "59da1e994e41f207e0d3628c426b3101",
     "url": "/assets/backgrounds/velasTarta2.jpg"
   },
   {
-    "revision": "0809713f0bcc64a60646122cb216aae2",
-    "url": "/assets/backgrounds/bordeChupetes.jpg"
+    "revision": "d8e2d32d448f0cf7231e01a18319afbd",
+    "url": "/assets/backgrounds/piesBebeYmanos.jpg"
   },
   {
-    "revision": "1d9c697ba3a449f3b9d7291113f52d7b",
-    "url": "/assets/backgrounds/velasTarta1.jpg"
+    "revision": "59347948cdeff2851065dc1cf028a712",
+    "url": "/assets/backgrounds/calendar.jpg"
   },
   {
     "revision": "931e25b2c8942555037eda01859db75b",
     "url": "/assets/backgrounds/logoV01.png"
   },
   {
-    "revision": "725e1f11703aabee8611c76d1990accb",
-    "url": "/assets/backgrounds/manos.jpg"
-  },
-  {
-    "revision": "fd571615708ccb5da331cda5851e9d7a",
-    "url": "/assets/backgrounds/marcoGlobos.png"
+    "revision": "1d9c697ba3a449f3b9d7291113f52d7b",
+    "url": "/assets/backgrounds/velasTarta1.jpg"
   },
   {
     "revision": "3c87be111e5f30c7b2282f345ac55b94",
@@ -160,40 +152,48 @@ self.__precacheManifest = [
     "url": "/assets/backgrounds/nubes.jpg"
   },
   {
+    "revision": "725e1f11703aabee8611c76d1990accb",
+    "url": "/assets/backgrounds/manos.jpg"
+  },
+  {
+    "revision": "fd571615708ccb5da331cda5851e9d7a",
+    "url": "/assets/backgrounds/marcoGlobos.png"
+  },
+  {
     "revision": "0869fb73d25bfa9b7e5181038966aa2e",
     "url": "/assets/backgrounds/paraPruebas.png"
-  },
-  {
-    "revision": "94d158fda1a61559e229fcea3b4014a8",
-    "url": "/assets/backgrounds/imgsfuente/baloonFreeNoOP.png"
-  },
-  {
-    "revision": "ffcde3ad040f88a45c1ed75e11f1f766",
-    "url": "/assets/backgrounds/sol.jpg"
-  },
-  {
-    "revision": "7b3a53f7c8911bfb86e14d4732267348",
-    "url": "/assets/backgrounds/regaloCumple.jpg"
-  },
-  {
-    "revision": "e6b565b977d10d619ea6d174ad9966c1",
-    "url": "/assets/backgrounds/baloonFree.png"
   },
   {
     "revision": "221f98ebe0295b92ebbde83e97b0f640",
     "url": "/assets/backgrounds/rezar.jpg"
   },
   {
+    "revision": "94d158fda1a61559e229fcea3b4014a8",
+    "url": "/assets/backgrounds/imgsfuente/baloonFreeNoOP.png"
+  },
+  {
     "revision": "700cd81ff6b11119e73340b00217f403",
     "url": "/assets/backgrounds/salonCelebracion2.jpg"
+  },
+  {
+    "revision": "e6b565b977d10d619ea6d174ad9966c1",
+    "url": "/assets/backgrounds/baloonFree.png"
   },
   {
     "revision": "7ebdabf70f9dea145b98493415670b2e",
     "url": "/assets/backgrounds/salonCelebracion1.jpg"
   },
   {
-    "revision": "da3dc9dc9d07c1276c310079ee37bfbd",
-    "url": "/assets/backgrounds/baloon-image-17.jpg"
+    "revision": "7b3a53f7c8911bfb86e14d4732267348",
+    "url": "/assets/backgrounds/regaloCumple.jpg"
+  },
+  {
+    "revision": "ffcde3ad040f88a45c1ed75e11f1f766",
+    "url": "/assets/backgrounds/sol.jpg"
+  },
+  {
+    "revision": "9f36d6fdedbcaf374db989e5e4c5e922",
+    "url": "/assets/backgrounds/up2.png"
   },
   {
     "revision": "014608ab19ff4903674c8804457ee753",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.482f59b35786037075510a61f1458427.js"
+  "/precache-manifest.9653dc7df0223a6cbdf169efde1d4483.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "invita"});
